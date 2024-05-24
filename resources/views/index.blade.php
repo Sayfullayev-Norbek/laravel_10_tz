3b2c3d62fc575ee0f6505ddf89f2d5ef3154c885
Sayfullayev Norbek <sayfullayev1572norbek@gmail.com>
2024-05-25 02:26:07 +0500

    Shablonni o'zimizga moslab olamiz

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends("layouts.site")
+
+@section('title')
+    Bosh sahifa
+@endsection
+
+@section("content")
+    @include('sections.mainPosts')
+    @include('sections.latestPosts')
+@endsection
